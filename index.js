7b1b3ff305f2f8aaeefb684c6fd2dff1518d2764
Yonus Billow <98870419+ybillow@users.noreply.github.com>
2023-02-23 18:48:24 +0000

    about page

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
-var http = require("http");
-http.createServer(function(req,res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Aloha world');
-}).listen(process.env.PORT || 3000);
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+  res.send('Ola Express!');
+});
+app.get('/about', (req, res) => {
+  res.send('About page!');
+});
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
